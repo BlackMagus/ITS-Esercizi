bad7224bd92e35adbe9660b07183f53832e22389
BlackMagus <alessiobasile.ab23@gmail.com>
2025-02-17 12:06:42 +0100

    Prova

--- Collection/provaN.py
+++ Collection/provaN.py
@@ -5,11 +5,45 @@ print("Una volta", famous_person, "disse: ", "Ho nel cuore 3 sentimenti con i qu
 
 names = ["Giuseppe", "Leandro", "Francesca"]
 message = {
-    "Giuseppe": "Ciao sono Giuseppe, per gli amici Peppe!",
-    "Leadro": "Ciao a Tutti!",
-    "Francesca": "Ciao Peppe!"
+    "Giuseppe": "Ciao sono Giuseppe, vorrei una panda",
+    "Leadro": "Ciao a Tutti! vorrei una moto",
+    "Francesca": "Ciao, vorrei un monopattino"
 }
 
 for names, message in message.items():
     print(f"{names}: {message}")
 
+
+Guest =["Francesca", "Alessandro", "Paolo"] 
+invite ={
+    "Francesca": "Sei invitata alla mia cena, non vedo l'ora di vederti!",
+    "Alessandro": "Ciao amico mio! Sei invitato a casa mia domani",
+    "Paolo": "Sei invitato alla mia cena domani"
+} 
+
+print(f"{Guest}: {invite}")
+
+
+Listaz = Guest.pop()
+
+
+
+print(sorted(names))
+names.reverse()
+print (names)
+len_invitati = len(Guest)
+for i in range(len_invitati)
+print (Guest[i])
+
+## 3.10
+
+Finale = ["Filippo", "Giorgio", "Dario"]
+print (f"{Finale}")
+
+messaggio = {
+    "Filippo": "Buongiorno a tutti!",
+    "Giorgio": "Buonasera a tutti!",
+    "Dario": "Buonanotte a tutti!"
+}
+
+print(sorted(messaggio))
